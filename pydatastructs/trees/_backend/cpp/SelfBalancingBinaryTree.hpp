5dc4f1f684ce3eba0aa75052af52befa3214dbb8
Kishan-Ved <kishan.ved@iitgn.ac.in>
2024-06-01 15:22:56 +0530

    Started C++ backend for self balancing binary tree

---
+++ pydatastructs/trees/_backend/cpp/SelfBalancingBinaryTree.hpp
@@ -0,0 +1,104 @@
+#ifndef TREES_SELFBALANCINGSelfBalancingBinaryTree_HPP
+#define TREES_SELFBALANCINGSelfBalancingBinaryTree_HPP
+
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <structmember.h>
+#include <cstdlib>
+#include "../../../utils/_backend/cpp/utils.hpp"
+#include "../../../utils/_backend/cpp/TreeNode.hpp"
+#include "../../../linear_data_structures/_backend/cpp/arrays/ArrayForTrees.hpp"
+#include "../../../linear_data_structures/_backend/cpp/arrays/DynamicOneDimensionalArray.hpp"
+#include "BinarySearchTree.hpp"
+
+typedef struct {
+    PyObject_HEAD
+    BinarySearchTree* bst;
+} SelfBalancingBinaryTree;
+
+static void SelfBalancingBinaryTree_dealloc(SelfBalancingBinaryTree *self) {
+    Py_TYPE(self)->tp_free(reinterpret_cast<PyObject*>(self));
+}
+
+static PyObject* SelfBalancingBinaryTree___new__(PyTypeObject* type, PyObject *args, PyObject *kwds) {
+    SelfBalancingBinaryTree *self;
+    self = reinterpret_cast<SelfBalancingBinaryTree*>(type->tp_alloc(type, 0));
+
+    if (PyType_Ready(&BinarySearchTreeType) < 0) { // This has to be present to finalize a type object. This should be called on all type objects to finish their initialization.
+        return NULL;
+    }
+    PyObject* p = BinarySearchTree___new__(&BinarySearchTreeType, args, kwds);
+    self->bst = reinterpret_cast<BinarySearchTree*>(p);
+
+    return reinterpret_cast<PyObject*>(self);
+}
+
+static PyObject* SelfBalancingBinaryTree___str__(SelfBalancingBinaryTree *self) {
+    return BinarySearchTree___str__(self->bst);
+}
+
+static PyObject* SelfBalancingBinaryTree_insert(SelfBalancingBinaryTree* self, PyObject* args) {
+    return BinarySearchTree_insert(self->bst, args);
+}
+
+static PyObject* SelfBalancingBinaryTree_search(SelfBalancingBinaryTree* self, PyObject *args, PyObject *kwds) {
+    return BinarySearchTree_search(self->bst, args, kwds);
+}
+
+static PyObject* SelfBalancingBinaryTree_delete(SelfBalancingBinaryTree* self, PyObject *args, PyObject *kwds) {
+    return BinarySearchTree_delete(self->bst, args, kwds);
+}
+
+static struct PyMethodDef SelfBalancingBinaryTree_PyMethodDef[] = {
+    {"insert", (PyCFunction) SelfBalancingBinaryTree_insert, METH_VARARGS | METH_KEYWORDS, NULL},
+    {"delete", (PyCFunction) SelfBalancingBinaryTree_delete, METH_VARARGS | METH_KEYWORDS, NULL},
+    {"search", (PyCFunction) SelfBalancingBinaryTree_search, METH_VARARGS | METH_KEYWORDS, NULL},
+    {NULL}
+};
+
+static PyMemberDef SelfBalancingBinaryTree_PyMemberDef[] = {
+    {NULL}  /* Sentinel */
+};
+
+
+static PyTypeObject SelfBalancingBinaryTreeType = {
+    /* tp_name */ PyVarObject_HEAD_INIT(NULL, 0) "SelfBalancingBinaryTree",
+    /* tp_basicsize */ sizeof(SelfBalancingBinaryTree),
+    /* tp_itemsize */ 0,
+    /* tp_dealloc */ (destructor) SelfBalancingBinaryTree_dealloc,
+    /* tp_print */ 0,
+    /* tp_getattr */ 0,
+    /* tp_setattr */ 0,
+    /* tp_reserved */ 0,
+    /* tp_repr */ 0,
+    /* tp_as_number */ 0,
+    /* tp_as_sequence */ 0,
+    /* tp_as_mapping */ 0,
+    /* tp_hash  */ 0,
+    /* tp_call */ 0,
+    /* tp_str */ (reprfunc) SelfBalancingBinaryTree___str__,
+    /* tp_getattro */ 0,
+    /* tp_setattro */ 0,
+    /* tp_as_buffer */ 0,
+    /* tp_flags */ Py_TPFLAGS_DEFAULT | Py_TPFLAGS_BASETYPE,
+    /* tp_doc */ 0,
+    /* tp_traverse */ 0,
+    /* tp_clear */ 0,
+    /* tp_richcompare */ 0,
+    /* tp_weaklistoffset */ 0,
+    /* tp_iter */ 0,
+    /* tp_iternext */ 0,
+    /* tp_methods */ SelfBalancingBinaryTree_PyMethodDef,
+    /* tp_members */ SelfBalancingBinaryTree_PyMemberDef,
+    /* tp_getset */ 0,
+    /* tp_base */ &BinarySearchTreeType,
+    /* tp_dict */ 0,
+    /* tp_descr_get */ 0,
+    /* tp_descr_set */ 0,
+    /* tp_dictoffset */ 0,
+    /* tp_init */ 0,
+    /* tp_alloc */ 0,
+    /* tp_new */ SelfBalancingBinaryTree___new__,
+};
+
+#endif
